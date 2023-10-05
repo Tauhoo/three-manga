f33d50eda4b4c7e1f650e57f4554fe58092f04d8
Tauhoo <tauhoo_ice@hotmail.com>
2023-10-05 22:00:02 +0700

    update tests to have the same result

--- tests/react-test/src/App.tsx
+++ tests/react-test/src/App.tsx
@@ -3,7 +3,6 @@ import './App.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { MangaDirectionalLight, MangaShaderManager } from 'three-manga'
-import { MangaMaterial } from '../../../dist/types/MangaMaterial'
 
 function App() {
   const containerRef = useRef<HTMLDivElement>(null)
@@ -27,60 +26,12 @@ function App() {
     const scene = sceneRef.current
     const controls = controlsRef.current
     const mangaShaderManager = mangaShaderManagerRef.current
-    // meshRef.current.rotation.y += 0.01
 
     controls.update()
     mangaShaderManager.update()
     renderer.render(scene, camera)
 
     window.requestAnimationFrame(animate)
-  }
-
-  function name() {
-    if (rendererRef.current !== null) return
-    if (cameraRef.current !== null) return
-    if (sceneRef.current !== null) return
-    if (controlsRef.current !== null) return
-    if (mangaShaderManagerRef.current !== null) return
-    if (containerRef.current == null) return
-
-    const renderer = new THREE.WebGLRenderer({ alpha: true })
-    renderer.pixelRatio = window.devicePixelRatio
-    renderer.shadowMap.enabled = true
-
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    renderer.pixelRatio = window.devicePixelRatio
-    containerRef.current.appendChild(renderer.domElement)
-
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      10000
-    )
-    const mangaLight = new MangaDirectionalLight(-2, 2, 2, -2, 1, 5)
-
-    const mangaShaderManager = new MangaShaderManager({
-      renderer: renderer,
-      scene: scene,
-      camera: camera,
-      lightList: [mangaLight],
-      resolution: new THREE.Vector2(window.innerWidth, window.innerHeight),
-    })
-    const material = mangaShaderManager.getMangaMaterial()
-
-    const geometry = new THREE.TorusKnotGeometry(0.6, 0.2, 400, 100)
-    const mesh = new THREE.Mesh(geometry, material)
-
-    camera.position.z = 3
-    mangaLight.position.z += 2
-    mangaLight.position.x += 2
-    mangaLight.position.y += 2
-    mangaLight.lookAt(mesh.position)
-
-    scene.add(mesh)
-    scene.add(mangaLight)
   }
 
   useEffect(() => {
@@ -107,27 +58,17 @@ function App() {
     )
     const controls = new OrbitControls(camera, renderer.domElement)
     const mangaLight = new MangaDirectionalLight(-2, 2, 2, -2, 1, 5)
-    const mangaLight2 = new MangaDirectionalLight(-2, 2, 2, -2, 1, 5)
-
-    const mangaLightHelper = new THREE.CameraHelper(mangaLight)
-    const mangaLight2Helper = new THREE.CameraHelper(mangaLight2)
 
     const mangaShaderManager = new MangaShaderManager({
       renderer: renderer,
       scene: scene,
       camera: camera,
-      lightList: [
-        mangaLight,
-        // mangaLight2
-      ],
+      lightList: [mangaLight],
       resolution: new THREE.Vector2(window.innerWidth, window.innerHeight),
       shadowDepthTexturepixelsPerUnit: 1024 * 2,
     })
-    const material = mangaShaderManager.getMangaMaterial({
-      shadowPattern: 1,
-      outlinePixelStep: 20,
-      inlinePixelStep: 10,
-    })
+    const material = mangaShaderManager.getMangaMaterial()
+
     const geometry = new THREE.TorusKnotGeometry(0.6, 0.2, 400, 100)
     const mesh = new THREE.Mesh(geometry, material)
     mesh.receiveShadow = true
@@ -139,16 +80,8 @@ function App() {
     mangaLight.position.y += 2
     mangaLight.lookAt(mesh.position)
 
-    mangaLight2.position.z -= 2
-    mangaLight2.position.x -= 2
-    mangaLight2.position.y += 2
-    mangaLight2.lookAt(mesh.position)
-
     scene.add(mesh)
     scene.add(mangaLight)
-    scene.add(mangaLight2)
-    scene.add(mangaLightHelper)
-    scene.add(mangaLight2Helper)
 
     rendererRef.current = renderer
     cameraRef.current = camera
